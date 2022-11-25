6872178d29a643d72fa7c07800cc6d8abb680676
Jonathan Zarate <zaratedev@gmail.com>
2022-11-25 12:32:39 -0600

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,7 @@
 
 namespace Zaratesystems\LaravelExcludable\Tests;
 
-use Illuminate\Routing\Router;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Zarate\Filterable\FilterableServiceProvider;
-use Zarate\Filterable\Tests\Filters\UserFilter;
-use Zarate\Filterable\Tests\Models\User;
 
 class TestCase extends Orchestra
 {
